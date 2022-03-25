c86182d5fb2e85203c6a0ee515c7034fbfe5696d
Smith <25jsmith@FVFZMYB5J1WK.tld>
2022-03-25 10:55:08 -0500

    First commit

---
+++ app.js
@@ -0,0 +1,38 @@
+function askName() {
+    var n = promt("Hello! What is your name?")
+    return n
+}
+
+function w(){
+ do{
+    var w = promt("Hello, would you like to convert the temperature to C to F or F to C.")
+    }while ((w !="F") && (w !="F"))
+    return w
+}
+
+function farenheit() {
+    if( w =="F") {
+        do{
+            var farenheitTemp = prompt("What is the weather in F")
+        }while (isNaN(weather)== true)
+        return farenheitTemp
+    }
+}
+ 
+
+function happy {
+    if(isNaN(w)== false)
+    var happy = farenheitTemp - 32 * 5 / 9
+    return happy
+}
+
+function celsius () {
+
+}
+
+
+
+
+
+
+
